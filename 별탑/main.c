cba8368173e7ae9671b573f6805a223f7d5ae96c
williameom <williameom5678@gmail.com>
2022-07-19 19:12:17 +0900

    22.07.19 끝

---
+++ 별탑/main.c
@@ -0,0 +1,15 @@
+#define _CRT_NO_SECURE_WARNINGS
+
+#include <stdio.h>
+int main(void)
+{
+	int x, y;
+	for (y = 1; y <= 5; y++)
+	{
+		for (x = 0; x < y; x++)
+			printf("*");
+		printf("\n");	
+	}
+
+	return 0;
+}
